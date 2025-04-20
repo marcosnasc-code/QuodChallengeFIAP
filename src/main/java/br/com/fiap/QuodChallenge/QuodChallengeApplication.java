7e59b237961c70a13f09d8102ea8f2d6daf1edff
marcosnasc-code <marcos.nacimento0321@gmail.com>
2025-04-19 23:16:01 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/QuodChallenge/QuodChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.QuodChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuodChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuodChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/QuodChallenge/QuodChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.QuodChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuodChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
